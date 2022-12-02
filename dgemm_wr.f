b4d7f31208ceb57cfefa5f1806938b30e24d8d83
Lucas Ramos Cardoso Tinoco Cortez <luccorte@gpu-20-2.localdomain>
2022-12-02 15:54:54 -0600

    Wrapper translated

--- dgemm_wr.f
+++ dgemm_wr.f
@@ -1,65 +1,20 @@
-      interface
-           integer(c_int) function &
-           cublasCreate_v2(handle) &
-           bind(c, name="cublasCreate_v2")
-        use, intrinsic :: iso_c_binding
-        type(c_ptr) :: handle
-      end function cublasCreate_v2
-  
-      integer(c_int) function &
-           cublasDestroy_v2(handle) &
-           bind(c, name="cublasDestroy_v2")
-        use, intrinsic :: iso_c_binding
-        type(c_ptr), value :: handle
-      end function cublasDestroy_v2
-  
-      integer(c_int) function &
-           cudaDeviceSynchronize_v2() &
-           bind(c, name="cudaDeviceSynchronize")
-        use, intrinsic :: iso_c_binding
-      end function cudaDeviceSynchronize_v2
-  
-      integer(c_int) function &
-           cublasDgemm_v2(handle, transa, transb, m, n, k, alpha, dA,&
-           ldda, dB, lddb, beta, dC, lddc) &
-           bind(c, name="cublasDgemm_v2")
-        use, intrinsic :: iso_c_binding
-        type(c_ptr), value :: handle
-        integer(c_int), value :: transa
-        integer(c_int), value :: transb
-        integer(c_int), value :: m
-        integer(c_int), value :: n
-        integer(c_int), value :: k
-        real(c_double) :: alpha
-        type(c_ptr), value :: dA
-        integer(c_int), value :: ldda
-        type(c_ptr), value :: dB
-        integer(c_int), value :: lddb
-        real(c_double) :: beta
-        type(c_ptr), value :: dC
-        integer(c_int), value :: lddc
-      end function cublasDgemm_v2
-  
-  
-      end interface
-
-      SUBROUTINE DGEMM_WR (cublas_handle,TA,TB,M,N,K
+      SUBROUTINE DGEMM_WR ( ,TA,TB,M,N,K
      2,AL,A,LDA,B,LDB,BT,C,LDC)
+      SUBROUTINE DGEMM_WR ( cublas_error, cublas_handle, TRANSA, TRANSB, M, N,
+     2                      K, ALPHA, A, LDA, B, LDB, BETA, C, LDC )
       use iso_c_binding
-c     .. Scalar Arguments ..
+c     .. Arguments ..
       implicit none
-      CHARACTER*1        TA, TB
-      real*8   AL, BT
-c     .. Array Arguments ..
-      integer(c_int)   :: cublas_error
+      real*8   ALPHA, BETA
+      integer(c_int)   :: cublas_error, TRANSA, TRANSB
       type(c_ptr) :: cublas_handle
 
       INTEGER            M, N, K, LDA, LDB, LDC
        real*8   A( LDA, * ), B( LDB, * ), C( LDC, * )
 
       !$omp target data use_device_ptr(aa,bb,cc_gpu)
-       cublas_error = cublasDgemm_v2(cublas_handle,1,1,M,N
-     2,K,AL,A,LDA,B,LDB,BT,C,LDC);
+       cublas_error = cublasDgemm_v2( cublas_handle, TRANSA, TRANSB, M, N,
+     2                                K, AL, A, LDA, B, LDB, BT, C, LDC);
       !$omp end target data
          if(cublas_error .ne. CUBLAS_STATUS_SUCCESS ) then
             print *, "failed", cublas_error, cc_gpu(1)
