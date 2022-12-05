4afa38ef5242c00e7b83f79d7f1c9e1ccf14ba90
Lucas Ramos Cardoso Tinoco Cortez <luccorte@gpu-20-8.localdomain>
2022-12-05 16:12:38 -0600

    removed lines

--- dgemm_wr.f
+++ dgemm_wr.f
@@ -15,16 +15,5 @@ c     .. Arguments ..
       !$omp target data use_device_ptr(aa,bb,cc_gpu)
        cublas_error = cublasDgemm_v2( cublas_handle, TRANSA, TRANSB, M, N,
      2                                K, AL, A, LDA, B, LDB, BT, C, LDC);
-      !$omp end target data
-         if(cublas_error .ne. CUBLAS_STATUS_SUCCESS ) then
-            print *, "failed", cublas_error, cc_gpu(1)
-            call exit(1)
-         endif
-      
-         ! wait for call to finish
-         cublas_error = cudaDeviceSynchronize_v2()
-         cublas_error = cublasDestroy_v2(cublas_handle)
-c      call dgemm(TA,TB,M,N,K,AL,A,LDA,B,LDB,BT,C,LDC)
-       
        return
        end
