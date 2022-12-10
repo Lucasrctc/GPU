af60aa8d46843446804f51d49cc362707caf72d8
Lucas Ramos Cardoso Tinoco Cortez <luccorte@gpu-21-16.localdomain>
2022-12-09 23:19:04 -0600

    Fixed some compilation errors

--- dgemm_wr.f
+++ dgemm_wr.f
@@ -1,19 +1,17 @@
-      SUBROUTINE DGEMM_WR ( ,TA,TB,M,N,K
-     2,AL,A,LDA,B,LDB,BT,C,LDC)
-      SUBROUTINE DGEMM_WR ( cublas_error, cublas_handle, TRANSA, TRANSB, M, N,
-     2                      K, ALPHA, A, LDA, B, LDB, BETA, C, LDC )
+      SUBROUTINE DGEMM_WR ( cublas_error, cublas_handle, TRANSA, TRANSB, M, N, &
+                          K, ALPHA, A, LDA, B, LDB, BETA, C, LDC )
       use iso_c_binding
-c     .. Arguments ..
+      ! .. Arguments ..
       implicit none
       real*8   ALPHA, BETA
       integer(c_int)   :: cublas_error, TRANSA, TRANSB
       type(c_ptr) :: cublas_handle
 
       INTEGER            M, N, K, LDA, LDB, LDC
-       real*8   A( LDA, * ), B( LDB, * ), C( LDC, * )
+      real*8   A( LDA, * ), B( LDB, * ), C( LDC, * )
 
       !$omp target data use_device_ptr(aa,bb,cc_gpu)
-       cublas_error = cublasDgemm_v2( cublas_handle, TRANSA, TRANSB, M, N,
-     2                                K, AL, A, LDA, B, LDB, BT, C, LDC);
-       return
-       end
+      cublas_error = cublasDgemm_v2( cublas_handle, TRANSA, TRANSB, M, N, &
+                                   K, AL, A, LDA, B, LDB, BT, C, LDC);
+      return
+      end
